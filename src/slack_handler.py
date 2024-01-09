8c86d2683ca0421d3a38e2921b805b9f7da74860
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-01-09 12:25:57 -0300

    Delete SlackMethods class and add definitions to SlackHandler

--- src/slack_handler.py
+++ src/slack_handler.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from slack_bolt import App
-from slack_sdk import WebClient
 import certifi
 from src.openai_handler import OpenAIHandler
 
@@ -10,7 +9,6 @@ os.environ["SSL_CERT_FILE"] = certifi.where()
 
 # Define global variables
 thread_messages = {}
-
 
 # TODO: maybe use a separate config file with all the dictionaries/descriptions
 
@@ -25,54 +23,21 @@ class SlackHandler:
         DALLE_MODEL,
         CHATGPT_MODEL,
     ):
-        self.open_ai = OpenAIHandler(OPENAI_API_KEY, DALLE_MODEL, CHATGPT_MODEL)
-
-        # Initialize Slack client
-        self.client = WebClient(token=SLACK_BOT_TOKEN)
-
-        # Define Slack app
+        self.openai = OpenAIHandler(OPENAI_API_KEY, DALLE_MODEL, CHATGPT_MODEL)
         self.app = App(signing_secret=SIGNING_SECRET, token=SLACK_BOT_TOKEN)
-
-        # Initialize instances for handling messages and mentions
-        self.slack = SlackMethods(self.app, self.client, allowed_users, self.open_ai)
+        self.allowed_users = allowed_users
 
         # Direct Message Event
         @self.app.event("message")
         def handle_direct_messages(body):
-            self.slack.handle_event(body)
+            self.handle_event(body)
 
         # App Mention Event
         @self.app.event("app_mention")
         def handle_app_mentions(body):
-            self.slack.handle_event(body)
+            self.handle_event(body)
 
-
-class SlackMethods:
-    def __init__(
-        self, app: App, client: WebClient, allowed_users, open_ai: OpenAIHandler
-    ):
-        self.app = app
-        self.client = client
-        self.allowed_users = allowed_users
-        self.open_ai = open_ai
-
-    def handle_event(self, body):
-        event = body["event"]
-        msg = event["text"]
-        channel_id = event["channel"]
-        thread_id = event.get("thread_ts", None) or event["ts"]
-        user_id = event.get("user")
-
-        # TODO: add logging information about the current event
-        logging.info("%s: %s" % (user_id, msg))
-
-        # Check if user is allowed
-        if user_id in self.allowed_users.values():
-            self.handle_default(msg, channel_id, thread_id)
-        else:
-            self.send_permission_denied(channel_id, thread_id)
-
-    def handle_default(self, msg, channel_id, thread_id):
+    def handle_message(self, msg, channel_id, thread_id):
         thread_history = thread_messages.get(thread_id, [])
         thread_history.append({"role": "user", "content": msg})
 
@@ -89,7 +54,7 @@ class SlackMethods:
         else:
             print("Failed to post the message.")
 
-        response_from_chatgpt = self.open_ai.send_message(thread_history)
+        response_from_chatgpt = self.openai.send_message(thread_history)
         if isinstance(response_from_chatgpt, str):
             thread_history.append(
                 {"role": "assistant", "content": response_from_chatgpt}
@@ -98,7 +63,7 @@ class SlackMethods:
 
         try:
             if isinstance(response_from_chatgpt, bytes):
-                self.client.files_upload_v2(
+                self.app.client.files_upload_v2(
                     channel=channel_id,
                     thread_ts=thread_id,
                     username="Dall-E",
@@ -115,11 +80,24 @@ class SlackMethods:
         except Exception as e:
             # TODO: use logger here
             print(f"Error posting message: {e}")
+        pass
 
-    def send_permission_denied(self, channel_id, thread_id):
-        self.app.client.chat_postMessage(
-            channel=channel_id,
-            text="El usuario solicitante no pertenece a la lista de\
-                 usuarios permitidos. Solicite permiso para utilizar la aplicación",
-            thread_ts=thread_id,
-        )
+    def handle_event(self, body):
+        event = body["event"]
+        msg = event["text"]
+        channel_id = event["channel"]
+        thread_id = event.get("thread_ts", None) or event["ts"]
+        user_id = event.get("user")
+
+        # TODO: add logging information about the current event
+        logging.info("%s: %s" % (user_id, msg))
+
+        # Check if user is allowed
+        if user_id in self.allowed_users.values():
+            self.handle_message(msg, channel_id, thread_id)
+        else:
+            self.app.client.chat_postMessage(
+                channel=channel_id,
+                text="El usuario solicitante no pertenece a la lista de usuarios permitidos. Solicite permiso para utilizar la aplicación",
+                thread_ts=thread_id,
+            )
